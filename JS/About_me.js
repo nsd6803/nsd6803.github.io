de3bb287a8350928114972801ddf52909f27adbd
nsd6803 <57258677+nsd6803@users.noreply.github.com>
2022-11-17 18:25:08 +0300

    Update About_me.js

--- JS/About_me.js
+++ JS/About_me.js
@@ -1,14 +1,6 @@
 var gallery = document.querySelector('#gallery');
 var getVal = function (elem, style) { return parseInt(window.getComputedStyle(elem).getPropertyValue(style)); };
 var getHeight = function (item) { return item.querySelector('.content').getBoundingClientRect().height; };
-var resizeAll = function () {
-    var altura = getVal(gallery, 'grid-auto-rows');
-    var gap = getVal(gallery, 'grid-row-gap');
-    gallery.querySelectorAll('.gallery-item').forEach(function (item) {
-        var el = item;
-        el.style.gridRowEnd = "span " + Math.ceil((getHeight(item) + gap) / (altura + gap));
-    });
-};
 gallery.querySelectorAll('img').forEach(function (item) {
     if (item.complete) {
         console.log(item.src);
@@ -22,4 +14,3 @@ gallery.querySelectorAll('img').forEach(function (item) {
         });
     }
 });
-
